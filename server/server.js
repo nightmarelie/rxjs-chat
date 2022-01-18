618d58df366b1deeffce0f0b7c668500a3d3761b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-01-18 15:50:18 -0600

    feat(server): and scripts section and bundler as middleware

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const http = require("http");
+const Bundler = require("parcel-bundler");
 
 const app = express();
+
+const bundler = new Bundler("client/index.html");
+app.use(bundler.middleware());
 
 const server = http.createServer(app);
 
